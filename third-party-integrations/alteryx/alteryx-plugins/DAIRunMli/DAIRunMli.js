19969a2b661976dfe961c16e885af4c713c31aef
Achraf Merzouki <achraf.merzouki@h2o.ai>
2021-03-26 12:57:59 -0400

    Add OAuth and update the yxi

--- third-party-integrations/alteryx/alteryx-plugins/DAIRunMli/DAIRunMli.js
+++ third-party-integrations/alteryx/alteryx-plugins/DAIRunMli/DAIRunMli.js
@@ -8,11 +8,34 @@ const displayStandardMliOptions = () => {
     }
 }
 
+const toggleOAuth = () => {
+    if (Alteryx.Gui.Manager.getDataItem('UseOAuth').getValue()) {
+        document.getElementById('usernameBox').style.display = 'none'
+        document.getElementById('passwordBox').style.display = 'none'
+
+        document.getElementById('endpointURL').style.display = 'initial'
+        document.getElementById('introspectionURL').style.display = 'initial'
+        document.getElementById('clientID').style.display = 'initial'
+        document.getElementById('refreshToken').style.display = 'initial'
+    } else {
+        document.getElementById('usernameBox').style.display = 'initial'
+        document.getElementById('passwordBox').style.display = 'initial'
+
+        document.getElementById('endpointURL').style.display = 'none'
+        document.getElementById('introspectionURL').style.display = 'none'
+        document.getElementById('clientID').style.display = 'none'
+        document.getElementById('refreshToken').style.display = 'none'
+    }
+}
+
 Alteryx.Gui.BeforeLoad = function (manager, AlteryxDataItems, json) {
     displayStandardMliOptions()
+    toggleOAuth()
 }
 
 Alteryx.Gui.AfterLoad = function (manager) {
     displayStandardMliOptions()
+    toggleOAuth()
     Alteryx.Gui.Manager.getDataItem('IsTimeSeries').registerPropertyListener('value', displayStandardMliOptions)
+    Alteryx.Gui.Manager.getDataItem('UseOAuth').registerPropertyListener('value', toggleOAuth)
 }
