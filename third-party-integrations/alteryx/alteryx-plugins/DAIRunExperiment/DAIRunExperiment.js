3c2098b92eb5f3ad74718ed1ebbb2f3c4e7dd421
Achraf Merzouki <achraf.merzouki@h2o.ai>
2021-03-26 12:40:33 -0400

    Add OAuth support

--- third-party-integrations/alteryx/alteryx-plugins/DAIRunExperiment/DAIRunExperiment.js
+++ third-party-integrations/alteryx/alteryx-plugins/DAIRunExperiment/DAIRunExperiment.js
@@ -8,6 +8,26 @@ const displayScoreSelect = () => {
     }
 }
 
+const toggleOAuth = () => {
+    if (Alteryx.Gui.Manager.getDataItem('UseOAuth').getValue()) {
+        document.getElementById('usernameBox').style.display = 'none'
+        document.getElementById('passwordBox').style.display = 'none'
+
+        document.getElementById('endpointURL').style.display = 'initial'
+        document.getElementById('introspectionURL').style.display = 'initial'
+        document.getElementById('clientID').style.display = 'initial'
+        document.getElementById('refreshToken').style.display = 'initial'
+    } else {
+        document.getElementById('usernameBox').style.display = 'initial'
+        document.getElementById('passwordBox').style.display = 'initial'
+
+        document.getElementById('endpointURL').style.display = 'none'
+        document.getElementById('introspectionURL').style.display = 'none'
+        document.getElementById('clientID').style.display = 'none'
+        document.getElementById('refreshToken').style.display = 'none'
+    }
+}
+
 Alteryx.Gui.BeforeLoad = function (manager, AlteryxDataItems, json) {
 
     const operationClassificationSelector = new AlteryxDataItems.StringSelector('ClassificationDropdown', {
@@ -28,9 +48,12 @@ Alteryx.Gui.BeforeLoad = function (manager, AlteryxDataItems, json) {
     manager.addDataItem(operationTimeSeriesSelector)
     manager.bindDataItemToWidget(operationTimeSeriesSelector, 'TimeSeriesDropdown')
     displayScoreSelect()
+    toggleOAuth()
 }
 
 Alteryx.Gui.AfterLoad = function (manager) {
     displayScoreSelect()
+    toggleOAuth()
     Alteryx.Gui.Manager.getDataItem('IsClassification').registerPropertyListener('value', displayScoreSelect)
+    Alteryx.Gui.Manager.getDataItem('UseOAuth').registerPropertyListener('value', toggleOAuth)
 }
